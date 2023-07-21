9bdf39325595e41d11c825897bfb914361c8329d
MilagrosMarkevicz <134464122+MilagrosMarkevicz@users.noreply.github.com>
2023-07-21 00:13:22 -0300

    Agrega funcionalidad de categorías a la app publicaciones

---
+++ src/publicaciones/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from publicaciones.models import Publicaciones
+from publicaciones.models import Categoria
+from publicaciones.models import Comentario
+
+# Register your models here.
+
+admin.site.register(Publicaciones)
+admin.site.register(Categoria)
+admin.site.register(Comentario)
