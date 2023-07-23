18a2d5cb05820b023ed1998d8684f3190d477922
Gimena Feck <gfeck.19@gmail.com>
2023-07-23 18:19:14 -0300

    ajustes y cambios en texto

--- src/core/urls.py
+++ src/core/urls.py
@@ -5,6 +5,7 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = [
     path('index/', views.indexView, name='index'),
     path ('publicaciones/', views.publicacionesView, name='publicaciones'),
+    path ('contacto/', views.contactoView, name= 'contacto'),
 ]
 
 urlpatterns += staticfiles_urlpatterns ()
