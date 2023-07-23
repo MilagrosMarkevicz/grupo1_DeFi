d53d01462bbbdc411c249346b73b87757822f72d
MilagrosMarkevicz <134464122+MilagrosMarkevicz@users.noreply.github.com>
2023-07-22 21:23:56 -0300

    fusione los cambios de la rama de dabi con los mios

--- src/publicaciones/admin.py
+++ src/publicaciones/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-<<<<<<< HEAD
 from publicaciones.models import Publicaciones
 from publicaciones.models import Categoria
 from publicaciones.models import Comentario
@@ -9,7 +8,5 @@ from publicaciones.models import Comentario
 admin.site.register(Publicaciones)
 admin.site.register(Categoria)
 admin.site.register(Comentario)
-=======
 
 # Register your models here.
->>>>>>> cd15e72e32af12862d36d4f127c991364e4d4009
