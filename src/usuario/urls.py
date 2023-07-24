9c67ba78b91ae95832867a0c3cc69f2b694f2662
David Cerdan <davidgabriel_0306@hotmail.com>
2023-07-24 15:15:05 -0300

    agregue la funcion de editar y eliminar usuarios

--- src/usuario/urls.py
+++ src/usuario/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from django.contrib.auth.views import LoginView, LogoutView
 
-from .views import RegistroView
+from .views import RegistroView, EliminarUsuario,EditarUsuario
 
 
 app_name= 'usuarios'
@@ -10,7 +10,9 @@ app_name= 'usuarios'
 url_patterns=[
     path('login/',LoginView.as_view(template_name = 'usuarios/login.html'),name = 'login'),
     path('logout/', LogoutView.as_view(next_page='../login'),name = 'logout'),
-    path('registrarse/', RegistroView.as_view(),name = 'registrarse')
+    path('registrarse/', RegistroView.as_view(),name = 'registrarse'),
+    path('editar_usuario/<int:pk>', EditarUsuario.as_view(),name = 'editar_usuario'),
+    path('eliminar_usuario/<int:pk>', EliminarUsuario.as_view(), name= 'eliminar_usuario')
 
 
 ]
--- src/usuario/views.py
+++ src/usuario/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
 
 from django.contrib.auth.forms import UserCreationForm
 
 from django.contrib.auth.models import User
+
+from django.contrib.auth.mixins import LoginRequiredMixin 
+
+from django.contrib.auth import login
+
+from django.urls import reverse
 
 # Create your views here.
 
@@ -16,3 +22,33 @@ class RegistroView(CreateView):
     form_class = UserCreationForm
 
 
+class EditarUsuario(UpdateView, LoginRequiredMixin):
+    model = User
+
+    template_name = 'usuarios/editar_usuario.html'
+
+    form_class = UserCreationForm
+
+
+    def form_valid(self, form):
+
+        respuesta = super().form_valid(form)
+        usuario = form.save()
+
+        login(self.request,usuario)
+
+        return respuesta
+
+
+    def get_success_url(self):
+        return reverse('index')
+    
+
+
+class EliminarUsuario(DeleteView, LoginRequiredMixin):
+    template_name = 'usuarios/eliminar_usuario.html'
+
+    model = User
+
+    def get_success_url(self):
+        return reverse('usuarios:login')
