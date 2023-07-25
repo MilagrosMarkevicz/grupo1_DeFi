6582ab489b14a90f21092e91687b49870cf71f3d
Luciano Marino <luciano.f.marino@gmail.com>
2023-07-24 23:22:24 -0300

    elimnando urls y views de usuarios

--- src/usuario/views.py
+++ src/usuario/views.py
@@ -1,15 +1,3 @@
 from django.shortcuts import render
 
 # Create your views here.
-
-def usuarioView(request):
-    return render(request, 'usuario.html', {})
-
-def registrarseView(request):
-    return render(request, 'registrarse.html', {})
-
-def iniciarSesionView(request):
-    return render(request, 'iniciar-sesion.html', {})
-
-def cerrarSesionView(request):
-    return render(request, 'cerrar-sesion.html', {})
