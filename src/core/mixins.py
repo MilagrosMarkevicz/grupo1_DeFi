253c4692a01e8960863a69f3dd885b8def28e3b6
MilagrosMarkevicz <134464122+MilagrosMarkevicz@users.noreply.github.com>
2023-07-26 11:05:04 -0300

    Estoy solucionando errores en la base de datos

---
+++ src/core/mixins.py
@@ -0,0 +1,21 @@
+from typing import Optional
+from django.contrib.auth.mixins import UserPassesTestMixin
+
+class SuperUsuarioAutorMixin(UserPassesTestMixin):
+    def test_func(self):
+        usuario = self.request.user
+        obj = self.get_object()
+
+
+#Acá entra si se trata de una publicación, por la columna creador
+        if hasattr(obj, 'creador'):
+            return usuario == obj.creador or usuario.is_superuser
+        
+#Acá entra si se trata de un comentario, por la columna autor
+        if hasattr(obj, 'autor'):
+            return usuario == obj.autor or usuario.is_superuser or usuario == obj.post.creador
+        
+
+class ColaboradorMixin(UserPassesTestMixin):
+    def test_func(self):
+        return self.request.user.es_colaborador or self.request.user.is_superuser
