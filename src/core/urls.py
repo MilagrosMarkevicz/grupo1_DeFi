8fe200725ea65d97700c9133cb3242c2cb3af3fd
MilagrosMarkevicz <134464122+MilagrosMarkevicz@users.noreply.github.com>
2023-07-26 00:17:13 -0300

    Update urls.py

--- src/core/urls.py
+++ src/core/urls.py
@@ -3,7 +3,7 @@ from core import views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
-    path('index/', views.indexView, name='index'),
+    path('', views.indexView, name='index'),
     path ('post/', views.postView, name='post'),
     path ('contacto/', views.contactoView, name= 'contacto'),
     path('usuarios/', include('usuarios.urls')),
