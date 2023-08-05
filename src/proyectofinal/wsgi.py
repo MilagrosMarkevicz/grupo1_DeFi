3af88e6f105685dc16f63b1ef9c0d477b03e9b91
gnux28 <juamlopez42@gmail.com>
2023-08-05 02:18:54 -0300

    deploy

--- src/proyectofinal/wsgi.py
+++ src/proyectofinal/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'proyectofinal.settings.prod.py')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'proyectofinal.settings.prod')
 
 application = get_wsgi_application()
